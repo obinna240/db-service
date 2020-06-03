8f0a24c5e2465b565de1595ed0aee819e9b269cd
Obinna Onyimadu <obinna240@yahoo.co.uk>
2020-06-03 16:22:20 +0100

    remove service discovery

--- src/main/java/com/brownbag/news/dbservice/DbServiceApplication.java
+++ src/main/java/com/brownbag/news/dbservice/DbServiceApplication.java
@@ -2,12 +2,10 @@ package com.brownbag.news.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Profile("prodmysql")
-@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.brownbag.news.dbservice.repository")
 public class DbServiceApplication {
